fc025672f377fb05a0bb13a0e3a1e4eeb4f8011c
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-12 22:37:21 +0400

    Fix variable issue

--- src/config/variables.config.js
+++ src/config/variables.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  MONGO_URI:
-    'mongodb+srv://yervand_stepanyan:mongoleyero@learningenglishdb-fgz2h.mongodb.net/LearningEnglishDB?retryWrites=true&w=majority',
-  JWT: 'dev-jwt'
+  MONGO_URI: process.env.Mongo_URL,
+  JWT: "dev-jwt"
 };
